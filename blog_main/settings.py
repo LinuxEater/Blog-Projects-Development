540992d94fbf7bfe0dd03919fa3f0186cbcfa987
LinuxEater <moisessouzasantos001@gmail.com>
2025-11-19 18:27:33 -0300

    Add Render deployment configuration (Procfile, gunicorn, static settings).

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -124,10 +124,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
-STATICFILES_DIRS = [ 'blog_main/static',]  # Custom static files directory]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [ BASE_DIR / 'blog_main' / 'static' ]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
