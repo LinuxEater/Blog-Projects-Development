3da51400c3d67ffde33fa8625c8ba4ed25337366
LinuxEater <moisessouzasantos001@gmail.com>
2025-11-20 19:58:55 -0300

    add staticfiles

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -139,4 +139,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
